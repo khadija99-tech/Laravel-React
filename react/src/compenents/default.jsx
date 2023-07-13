7103f1893b9bdb11d08d59190bda56987ad08fcc
Khadija <khadijaelhabchi99@gmail.com>
2023-07-13 12:36:24 +0100

    fixing_dashboard

--- react/src/compenents/default.jsx
+++ react/src/compenents/default.jsx
@@ -1,5 +1,5 @@
-import { Outlet } from 'react-router-dom'
-
+import { Navigate, Outlet } from 'react-router-dom'
+import  {useStateContext} from '../contexts/provider'
 import React from 'react'
 
 export default function Default() {
